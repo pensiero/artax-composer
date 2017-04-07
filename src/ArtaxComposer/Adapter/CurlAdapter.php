ea630c430560e57faf7b3d4e41d5d37f248126f5
pensiero <oscar.nesis@gmail.com>
2017-04-08 01:11:07 +0200

    Base code for CurlAdapter

---
+++ src/ArtaxComposer/Adapter/CurlAdapter.php
@@ -0,0 +1,30 @@
+<?php
+namespace ArtaxComposer\Adapter;
+
+class CurlAdapter extends BaseAdapter implements AdapterInterface
+{
+    public function doRequest()
+    {
+        // TODO: Implement doRequest() method.
+    }
+
+    public function getResponseStatusCode()
+    {
+        // TODO: Implement getResponseStatusCode() method.
+    }
+
+    public function getResponseBody()
+    {
+        // TODO: Implement getResponseBody() method.
+    }
+
+    public function hasResponseHeader($header)
+    {
+        // TODO: Implement hasResponseHeader() method.
+    }
+
+    public function getResponseHeader($header)
+    {
+        // TODO: Implement getResponseHeader() method.
+    }
+}
